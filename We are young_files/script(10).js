ab9636776cee264162b03c355398a2985eb33b65
vincent <vincent@invokemalaysia.org>
2020-09-16 16:41:30 +0800

    Initial commit

---
+++ We are young_files/script(10).js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-5',
+          componentName:'DynamicFontScaler',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:false
+        });
+      })({"font":"primary","text":"We ARE YOung","containerId":"tagline-container-160697","targetId":"dynamic-tagline-160698","fontSizes":["xxxlarge","xxlarge","xlarge"],"maxLines":4,"style":{"display":"inline-block","maxWidth":"100%","whiteSpace":"pre-line","borderBottomWidth":"2px","borderBottomStyle":"solid","borderColor":"highContrastOverlay","paddingBottom":"xsmall","@sm":{"borderBottomWidth":0,"paddingBottom":0},"@xs-only":{"maxWidth":"100%","marginBottom":"xsmall","wordWrap":"break-word","overflowWrap":"break-word"}},"typography":"HeadingAlpha","widgetId":"fbdec8e3-e483-4850-9e5e-d0c2f6f0ccd6","section":"default","category":"accent","locale":"en-MY","renderMode":"PUBLISH"},{"widgetId":"fbdec8e3-e483-4850-9e5e-d0c2f6f0ccd6","widgetType":"HEADER","widgetPreset":"header9","section":"default","category":"accent","fontSize":"xxxlarge","fontFamily":"primary","group":"Hero","groupType":"Default","websiteThemeOverrides":{},"widgetThemeOverrides":{}});
